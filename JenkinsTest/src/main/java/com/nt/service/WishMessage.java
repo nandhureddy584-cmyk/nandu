f25b3e87e82b67d9c236b9c6b2c662e0dc45e549
nandhureddy584-cmyk <nandhureddy5842gmail.com>
2026-09-07 18:47:44 +0530

    commit

---
+++ JenkinsTest/src/main/java/com/nt/service/WishMessage.java
@@ -0,0 +1,44 @@
+package com.nt.service;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalTime;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/wish")
+public class WishMessage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("WishMessage.doGet()");
+		PrintWriter pw=res.getWriter();
+		res.setContentType("text/html");
+		LocalTime ltime=LocalTime.now();
+		int hour=ltime.getHour();
+		if(hour<12)
+			pw.println("<h1 style='color:red;align:center'>Good Morning</h1>");
+		else if(hour<16)
+			pw.println("<h1 style='color:red;align:center'>Good Afternoon</h1>");
+		else
+			pw.println("<h1 style='color:red;align:center'>Good Evening</h1>");
+		
+		
+		pw.println("<br> <a href='index.jsp'>Home</a>");
+		pw.close();
+		
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		System.out.println("WishMessage.doPost()");
+		doGet(req,res);
+	}
+
+	
+	
+}
---
+++ JenkinsTest/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+
+
+
+<h1 style="color:red;text-align:center">jenkins Maven WebApp</h1>
+<br><br>
+<a href="wish">Generate Wish message</a>
